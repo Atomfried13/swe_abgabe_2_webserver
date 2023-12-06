acc6a78c13e6da036c0e6539e4b9da26b1f92c4c
Konstantin <krausko@outlook.de>
2023-12-06 19:46:49 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-// eslint-disable-next-line eslint-comments/disable-enable-pair
-/* eslint-disable react/react-in-jsx-scope */
-import react from 'react';
+import React from 'react';
 import reactDOM from 'react-dom/client';
 import { App } from './App.tsx';
 import './index.css';
 
 reactDOM.createRoot(document.getElementById('root')!).render(
-	<react.StrictMode>
+	<React.StrictMode>
 		<App/>
-	</react.StrictMode>,
+	</React.StrictMode>,
 );
