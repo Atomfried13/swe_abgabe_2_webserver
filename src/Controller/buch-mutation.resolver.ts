85fcb4e86c4cc4544a6b8e6deb582c73d797b18d
Konstantin <krausko@outlook.de>
2023-12-06 19:45:20 +0100

    Update buch-mutation.resolver.ts

--- src/Controller/buch-mutation.resolver.ts
+++ src/Controller/buch-mutation.resolver.ts
@@ -1,40 +1,37 @@
-/* eslint-disable eslint-comments/disable-enable-pair */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { BuchDTOohneref } from './buchDTO.entitie';
 import { axiosInstance } from './getAxiosInstance';
 
 export const mutation = async (buchohneref: BuchDTOohneref) => {
-	const response = await axiosInstance.post('baseURL/graphql', {
-		mutation: `   
-		create(
-			input: {
-				isbn: ${buchohneref.isbn},
-      			rating: ${buchohneref.isbn},
-      			art: ${buchohneref.isbn},
-      			preis: ${buchohneref.isbn},
-      			rabatt: ${buchohneref.isbn},
-      			lieferbar: ${buchohneref.isbn},
-      			datum: ${buchohneref.isbn},
-      			homepage: ${buchohneref.isbn},
-      			schlagwoerter: ${buchohneref.isbn},
-      			titel: {
-       				 titel: "Titelcreatemutation",
-        			untertitel: "untertitelcreatemutation"
-      			},
-      			abbildungen: [{
-        		beschriftung: "Abb. 1",
-        		contentType: "img/png"
-     			}]
-    		}
-  		) {
-      		id
-  		}
-		}`,
-	});
-	if (response === null || response === undefined) {
-		return 'Fehler';
+	try{
+		await axiosInstance.post('baseURL/graphql', {
+			mutation: `   
+			create(
+				input: {
+					isbn: ${buchohneref.isbn},
+					rating: ${buchohneref.isbn},
+					art: ${buchohneref.isbn},
+					preis: ${buchohneref.isbn},
+					rabatt: ${buchohneref.isbn},
+					lieferbar: ${buchohneref.isbn},
+					datum: ${buchohneref.isbn},
+					homepage: ${buchohneref.isbn},
+					schlagwoerter: ${buchohneref.isbn},
+					titel: {
+						titel: "Titelcreatemutation",
+						untertitel: "untertitelcreatemutation"
+					},
+					abbildungen: [{
+					beschriftung: "Abb. 1",
+					contentType: "img/png"
+					}]
+				}
+			) {
+				id
+			}
+			}`,
+		});
+	} catch (err: unknown) {
+		return 'Fehler beim Schreiben';
 	}
 	return 'Neues Buch angelegt.';
 };
