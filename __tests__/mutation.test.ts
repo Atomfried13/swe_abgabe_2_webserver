28e1b55a8ffe9807ce9f5db60a827714bcadccfc
Konstantin <krausko@outlook.de>
2023-12-07 12:57:12 +0100

    Update mutation.test.ts

--- __tests__/mutation.test.ts
+++ __tests__/mutation.test.ts
@@ -8,4 +8,39 @@ import { loginGraphQL } from '../login.js';
 describe('GraphQL Mutations', () => {
     let client: AxiosInstance;
     const graphqlPath = 'graphql';
+
+    beforeAll(async () => {
+        const baseURL = `https://localhost:3000/`;
+        client = axios.create({
+            baseURL,
+            httpsAgent,
+        });
+    });
+
+    test('Neues Buch', async () => {
+        // given
+        const token = await loginGraphQL(client);
+        const authorization = { Authorization: `Bearer ${token}` }; // eslint-disable-line @typescript-eslint/naming-convention
+        
+
+        // when
+        const response: AxiosResponse<GraphQLResponseBody> = await client.post(
+            graphqlPath,
+            body,
+            { headers: authorization },
+        );
+
+        // then
+        const { status, headers, data } = response;
+
+        expect(status).toBe(HttpStatus.OK);
+        expect(headers['content-type']).toMatch(/json/iu); // eslint-disable-line sonarjs/no-duplicate-string
+        expect(data.data).toBeDefined();
+
+        const { create } = data.data!;
+
+        // Der Wert der Mutation ist die generierte ID
+        expect(create).toBeDefined();
+        expect(create.id).toBeGreaterThan(0);
+    });
 });
