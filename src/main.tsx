6269e52a0c05f347fc79b3aaa01111f8ae2ee10c
Konstantin <krausko@outlook.de>
2023-12-16 15:12:34 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
 import './index.css';
+import { BuchSuchen } from './pages/BuchSuchen/BuchSuchen.tsx';
+import { Home } from './pages/Home/Home.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
+		<Home />
 		<App />
+		<BuchSuchen />
 	</React.StrictMode>,
 );
