c4f27491be33f8a6e441e47af4f55f41e2cecef8
Konstantin <krausko@outlook.de>
2023-12-16 13:56:43 +0100

    Eslint Anpassungen
    Nach Merge von Branch ohne Eslint

--- src/pages/BuchSuchen/BuchSuchen.tsx
+++ src/pages/BuchSuchen/BuchSuchen.tsx
@@ -1,80 +1,68 @@
-import { useState } from 'react';
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable max-lines-per-function */
+import { Key, useState } from 'react';
 import { Form, Button, Table } from 'react-bootstrap';
 import './BuchSuchen.css';
-import { axiosInstance } from '../../Controller/getAxiosInstance';
+import { BuchDTO } from '../../Model/buchDTO.entitie';
+import { fetchTitel } from '../../Controller/buch-query';
 
-export function BuchSuchen(){
+export function BuchSuchen() {
 	const [data, set] = useState(null);
 	const [titel, setTitel] = useState('');
 	const [showTable, setShowTable] = useState(false);
-	
-	const fetchTitel = async () => {
-		try {
-			const response = await axiosInstance.post('baseURL/query', {
-				variables: { titel }, //a??
-				query: `
-		query ($titel: String) {
-			buecher(titel: $titel) {
-			isbn
-			art
-			preis
-			schlagwoerter
-			rating
-			titel {
-			  titel
-			}
-		  }
-		}
-		`,
-			});
-			set(response.data);
-			setShowTable(true);
-			console.log(response.data);
-		} catch (err: unknown) {
-			console.log('Fehler, genauere Fehlermeldung noch nicht vorhanden, schaue in die Konsole des Browsers');
-		} // Fehlermedlung noch ausbauen !!!!!!!
-	};
 
-	const handleSearchClick_Titel = () => {
-		fetchTitel();
+	const handleSearchClickTitel = async () => {
+		set(await fetchTitel(titel));
+		setShowTable(true);
 	};
 
 	return (
-		<><div className="d-flex align-items-center">
-			<Form>
-				<Form.Group className="buch-suchen-form" controlId="formGroupSuchen">
-					<Form.Control 
-						type="suchkriterien" 
-						placeholder="Suche anhand des Titels..." 
-						value={titel}
-						onChange={(event) => setTitel(event.target.value)}
-					/>
-				</Form.Group>
-			</Form>
-			<Button onClick={handleSearchClick_Titel} className="suchen-btn">Suchen</Button>
-		</div>
-		{showTable && data &&
-		<Table striped bordered hover>
-			<thead>
-				<tr>
-					<th>Nr.</th>
-					<th>Titel</th>
-					<th>Preis</th>
-					<th>Art</th>
-					<th>Bewertung</th>
-				</tr>
-			</thead>
-			<tbody>
-				{data?.data.buecher.map((buch, index) => (
-					<tr key={index}>
-						<td>{index + 1}</td>
-						<td>{buch.titel?.titel}</td>
-						<td>{buch.preis}</td>
-						<td>{buch.art}</td>
-						<td>{buch.rating}</td>
-					</tr>
-				))}
-			</tbody>
-		</Table>}</>
+		<>
+			<div className="d-flex align-items-center">
+				<Form>
+					<Form.Group
+						className="buch-suchen-form"
+						controlId="formGroupSuchen"
+					>
+						<Form.Control
+							type="suchkriterien"
+							placeholder="Suche anhand des Titels..."
+							value={titel}
+							onChange={(event) => setTitel(event.target.value)}
+						/>
+					</Form.Group>
+				</Form>
+				<Button onClick={handleSearchClickTitel} className="suchen-btn">
+					Suchen
+				</Button>
+			</div>
+			{showTable && data && (
+				<Table striped bordered hover>
+					<thead>
+						<tr>
+							<th>Nr.</th>
+							<th>Titel</th>
+							<th>Preis</th>
+							<th>Art</th>
+							<th>Bewertung</th>
+						</tr>
+					</thead>
+					<tbody>
+						{data?.data.buecher.map((buch: BuchDTO, index: Key) => (
+							<tr key={index}>
+								<td>{index + 1}</td>
+								<td>{buch.titel?.titel}</td>
+								<td>{buch.preis}</td>
+								<td>{buch.art}</td>
+								<td>{buch.rating}</td>
+							</tr>
+						))}
+					</tbody>
+				</Table>
+			)}
+		</>
 	);
 }
--- src/pages/NeuesBuch/NeuesBuch.tsx
+++ src/pages/NeuesBuch/NeuesBuch.tsx
@@ -1,24 +1,34 @@
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable eslint-comments/no-duplicate-disable */
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable eslint-comments/no-duplicate-disable */
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable max-lines-per-function */
 import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import './NeuesBuch.css';
 
-export function NeuesBuch(){
+export function NeuesBuch() {
 	const [isbn, setISBN] = useState('');
 	const [titel, setTitel] = useState('');
 	const [preis, setPreis] = useState('');
 	const [rabatt, setRabatt] = useState('');
-	
+
 	const handleCreateClick = () => {
-		//Methode aufrufen mit der ein HTTP Request an die GraphQL Schnittstelle zum Neuanlegen gemacht wird 
+		//Methode aufrufen mit der ein HTTP Request an die GraphQL Schnittstelle zum Neuanlegen gemacht wird
 	};
 
-	return(
+	return (
 		<div>
 			<div className="infos">
 				<h2>Neuanlegen eines Buches</h2>
 			</div>
 			<Form onSubmit={handleCreateClick}>
-				<Form.Group controlId='validationISBN'>
+				<Form.Group controlId="validationISBN">
 					<Form.Label>ISBN</Form.Label>
 					<Form.Control
 						required
@@ -28,7 +38,7 @@ export function NeuesBuch(){
 						onChange={(event) => setISBN(event.target.value)}
 					/>
 				</Form.Group>
-				<Form.Group controlId='validationISBN'>
+				<Form.Group controlId="validationISBN">
 					<Form.Label>Titel</Form.Label>
 					<Form.Control
 						required
@@ -38,7 +48,7 @@ export function NeuesBuch(){
 						onChange={(event) => setTitel(event.target.value)}
 					/>
 				</Form.Group>
-				<Form.Group controlId='validationISBN'>
+				<Form.Group controlId="validationISBN">
 					<Form.Label>Preis</Form.Label>
 					<Form.Control
 						required
@@ -48,7 +58,7 @@ export function NeuesBuch(){
 						onChange={(event) => setPreis(event.target.value)}
 					/>
 				</Form.Group>
-				<Form.Group controlId='validationISBN'>
+				<Form.Group controlId="validationISBN">
 					<Form.Label>Rabatt (in Prozent)</Form.Label>
 					<Form.Control
 						required
@@ -58,7 +68,9 @@ export function NeuesBuch(){
 						onChange={(event) => setRabatt(event.target.value)}
 					/>
 				</Form.Group>
-				<Button type="submit" className="neuanlegen-btn">Neues Buch anlegen</Button>
+				<Button type="submit" className="neuanlegen-btn">
+					Neues Buch anlegen
+				</Button>
 			</Form>
 		</div>
 	);
