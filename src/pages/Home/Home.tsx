57bef69bfec2f4d37fc5b189149f3ede7a1845e8
Konstantin <krausko@outlook.de>
2023-12-17 14:46:37 +0100

    Routing Button in Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,7 @@
-/* eslint-disable eslint-comments/disable-enable-pair */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { Button } from 'react-bootstrap';
 import './Home.css';
-import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { LoggedOutNavbar } from '../Navigation/LoggedOutNavbar/Navbar';
-import { LoggedInNavbar } from '../Navigation/LoggedInNavbar/Navbar';
-import { BuchSuchen } from '../BuchSuchen/BuchSuchen';
-import { Login } from '../Login/Login';
-import { NeuesBuch } from '../NeuesBuch/NeuesBuch';
-import { Einloggen } from '../../Controller/auth.service';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export function Home() {
 	return (
@@ -23,9 +12,11 @@ export function Home() {
 					Erhalte einen umfassenden Überblick über alle Bücher und
 					verwalte deine Buchsammlung.
 				</p>
-				<Button type="submit" className="suchen-btn">
-					Bücher suchen
-				</Button>
+				<LinkContainer to="/BuchSuchen">
+					<Button type="submit" className="suchen-btn">
+						Bücher suchen
+					</Button>
+				</LinkContainer>
 			</div>
 		</div>
 	);
