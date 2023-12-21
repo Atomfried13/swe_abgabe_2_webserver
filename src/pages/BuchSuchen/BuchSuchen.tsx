73adf98fedb5a910e031269f216ecb6b7782b30c
Atomfried13 <147507681+Atomfried13@users.noreply.github.com>
2023-12-21 15:46:19 +0100

    Typproblematik für die Eigenschaft data gelöst

--- src/pages/BuchSuchen/BuchSuchen.tsx
+++ src/pages/BuchSuchen/BuchSuchen.tsx
@@ -12,17 +12,43 @@ import { Form, Button, Table, Alert } from 'react-bootstrap';
 import './BuchSuchen.css';
 import { fetchTitel, fetchId } from '../../Controller/buch-query';
 
+interface BuchData {
+	id: string;
+	isbn: string;
+	version: number;
+	preis: number;
+	rating: number;
+	art: string;
+	rabatt: string;
+	titel: {
+		titel: string;
+	};
+}
+
+interface QueryResultId {
+	data: {
+		buch: BuchData;
+	};
+}
+interface QueryResultTitel {
+	data: {
+		buecher: BuchData[];
+	};
+}
+
 export function BuchSuchen() {
-	const [data, setData] = useState(null);
+	const [datenTitel, setDatenTitel] = useState<QueryResultTitel | null>(null);
+	const [datenId, setDatenId] = useState<QueryResultId | null>(null);
 	const [searchTerm, setSearchTerm] = useState('');
 	const [showTableTitel, setShowTableTitel] = useState(false);
 	const [showTableId, setShowTableId] = useState(false);
 	const [error, setError] = useState('');
 
+	// eslint-disable-next-line max-statements
 	const handleSearchClick = async () => {
 		try {
 			if (!searchTerm) {
-				setData(await fetchTitel(searchTerm));
+				setDatenTitel(await fetchTitel(searchTerm));
 				setError('');
 				setShowTableId(false);
 				setShowTableTitel(true);
@@ -32,29 +58,34 @@ export function BuchSuchen() {
 
 				if (result.data.buecher !== null) {
 					setError('');
-					setData(result);
+					setDatenTitel(result);
 					setShowTableTitel(true);
 				} else {
 					setError('Mach kein Scheiße, gib was Gescheites an');
-					setData(null);
+					setDatenTitel(null);
 				}
 			} else {
 				const result = await fetchId(searchTerm);
 				setShowTableTitel(false);
 
-				if (result.data.buch !== null) {
+				if (
+					result.data.buch !== null &&
+					result.data !== null &&
+					result.data !== undefined
+				) {
 					setError('');
-					setData(result);
+					setDatenId(result);
 					setShowTableId(true);
 				} else {
 					setError('Mach kein Scheiße, gib was Gescheites an');
-					setData(null);
+					setDatenId(null);
 				}
 			}
 		} catch (error) {
 			console.error('Fehler beim Laden der Daten:', error);
 			setError('Fehler beim Laden der Daten');
-			setData(null);
+			setDatenId(null);
+			setDatenTitel(null);
 		}
 	};
 	return (
@@ -94,7 +125,7 @@ export function BuchSuchen() {
 					<p>{error}</p>
 				</Alert>
 			)}
-			{showTableTitel && data && (
+			{showTableTitel && datenTitel && (
 				<Table striped bordered hover>
 					<thead>
 						<tr>
@@ -106,7 +137,7 @@ export function BuchSuchen() {
 						</tr>
 					</thead>
 					<tbody>
-						{data?.data.buecher.map((buch, index) => (
+						{datenTitel?.data.buecher.map((buch, index) => (
 							<tr key={index}>
 								<td>{index + 1}</td>
 								<td>{buch.titel?.titel}</td>
@@ -118,7 +149,7 @@ export function BuchSuchen() {
 					</tbody>
 				</Table>
 			)}
-			{showTableId && data && (
+			{showTableId && datenId && (
 				<Table striped bordered hover>
 					<thead>
 						<tr>
@@ -130,12 +161,12 @@ export function BuchSuchen() {
 						</tr>
 					</thead>
 					<tbody>
-						<tr key={data.data.buch.id}>
-							<td>{data.data.buch.id}</td>
-							<td>{data.data.buch.titel?.titel}</td>
-							<td>{data.data.buch.preis}</td>
-							<td>{data.data.buch.art}</td>
-							<td>{data.data.buch.rating}</td>
+						<tr key={datenId.data.buch.id}>
+							<td>{datenId.data.buch.id}</td>
+							<td>{datenId.data.buch.titel?.titel}</td>
+							<td>{datenId.data.buch.preis}</td>
+							<td>{datenId.data.buch.art}</td>
+							<td>{datenId.data.buch.rating}</td>
 						</tr>
 					</tbody>
 				</Table>
