61e4d1c478c05a456a5ee9e07f1b7f8767daf5fc
Konstantin <krausko@outlook.de>
2023-12-22 23:40:32 +0100

    Funktionierende Mutation
    ISBN wird erfolgreich übergeben, bleibendes Problem wie bekommen wir die Buchart ohne String anführungszeichen in die mutation

--- src/Controller/buch-mutation.ts
+++ src/Controller/buch-mutation.ts
@@ -1,4 +1,8 @@
 /* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable eslint-comments/no-duplicate-disable */
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable eslint-comments/disable-enable-pair */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { BuchDTO } from '../Model/buchDTO.entitie';
@@ -9,32 +13,36 @@ export const mutation = async (buch: BuchDTO, token: string) => {
 	const authorization = { Authorization: token ? `Bearer ${token}` : '' };
 	try {
 		console.log('am anfang');
+		console.log(buch.isbn);
+		const isbn = buch.isbn;
+		console.log(isbn);
 		const response = await axiosInstance.post(
 			'baseURL/graphql',
 			{
+				variables: { isbn },
 				query: `
-				mutation {
-					create(
-					  input: {
-						isbn: "978-0-321-19368-1",
-						rating: 1,
-						art: KINDLE,
-						preis: 99.99,
-						rabatt: 0.123,
-						lieferbar: true,
-						datum: "2022-01-31",
-						homepage: "https://create.mutation",
-						schlagwoerter: ["JAVASCRIPT", "TYPESCRIPT"],
-						titel: {
-						  titel: "Titelcreatemutation",
-						  untertitel: "untertitelcreatemutation"
-						},
-						abbildungen: [{
-						  beschriftung: "Abb. 1",
-						  contentType: "img/png"
-						}]
-					  }
-					) {
+				mutation ($isbn: String!) {
+					create (input: 
+						{
+							isbn: $isbn,
+							rating: 1,
+							art: KINDLE,
+							preis: 99.99,
+							rabatt: 0.123,
+							lieferbar: true,
+							datum: "2022-01-31",
+							homepage: "https://create.mutation",
+							schlagwoerter: ["JAVASCRIPT", "TYPESCRIPT"],
+							titel: {
+							  titel: "Titelcreatemutation",
+							  untertitel: "untertitelcreatemutation"
+							},
+							abbildungen: [{
+							  beschriftung: "Abb. 1",
+							  contentType: "img/png"
+							}]
+						  }
+						) {
 						id
 					}
 				  }
