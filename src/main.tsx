d76e0b324d6702d1d1b517308b23e6289ed4b818
melvin1203 <147506157+melvin1203@users.noreply.github.com>
2023-12-30 16:50:48 +0100

    Änderung von allowedRoles für die Seite NeuesBuch

--- src/main.tsx
+++ src/main.tsx
@@ -25,7 +25,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 						element={
 							<RequireAuth
 								element={<NeuesBuch />}
-								allowedRoles={['kunde']}
+								allowedRoles={['admin', 'fachabteilung']}
 							/>
 						}
 					/>
