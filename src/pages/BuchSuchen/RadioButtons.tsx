54ec02c583e4b1899fc740af897c49c8568e16fb
Atomfried13 <147507681+Atomfried13@users.noreply.github.com>
2023-12-31 23:44:10 +0100

    Change: function statt const

--- src/pages/BuchSuchen/RadioButtons.tsx
+++ src/pages/BuchSuchen/RadioButtons.tsx
@@ -6,25 +6,30 @@ interface SearchRadioButtonsProps {
 	handleRadioClick: (letter: string) => void;
 }
 
-export const SearchRadioButtons = ({ selectedLetter, handleRadioClick }: SearchRadioButtonsProps) => (
-	<Form.Group>
-		<Form.Check
-			inline
-			type="radio"
-			label="A"
-			name="searchLetter"
-			id="searchLetterA"
-			onChange={() => handleRadioClick('A')}
-			checked={selectedLetter === 'A'}
-		/>
-		<Form.Check
-			inline
-			type="radio"
-			label="L"
-			name="searchLetter"
-			id="searchLetterL"
-			onChange={() => handleRadioClick('L')}
-			checked={selectedLetter === 'L'}
-		/>
-	</Form.Group>
-);
+export function SearchRadioButtons({
+	selectedLetter,
+	handleRadioClick,
+}: SearchRadioButtonsProps) {
+	return (
+		<Form.Group>
+			<Form.Check
+				inline
+				type="radio"
+				label="A"
+				name="searchLetter"
+				id="searchLetterA"
+				onChange={() => handleRadioClick('A')}
+				checked={selectedLetter === 'A'}
+			/>
+			<Form.Check
+				inline
+				type="radio"
+				label="L"
+				name="searchLetter"
+				id="searchLetterL"
+				onChange={() => handleRadioClick('L')}
+				checked={selectedLetter === 'L'}
+			/>
+		</Form.Group>
+	);
+}
