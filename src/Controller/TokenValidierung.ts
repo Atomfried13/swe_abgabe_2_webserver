af4477896ab7dad38573289df6a7f4dae32d9240
Melvin <147506157+melvin1203@users.noreply.github.com>
2024-01-04 20:13:56 +0100

    refactor: finale Anpassungen für Login

--- src/Controller/TokenValidierung.ts
+++ src/Controller/TokenValidierung.ts
@@ -5,15 +5,14 @@ export function isTokenExpired(
 	if (!expirationString) {
 		return true;
 	} else {
-		let expiresIn = 0;
-		const value = parseInt(expirationString);
+		let expiresIn = parseInt(expirationString);
 
 		if (expirationString.includes('h')) {
-			expiresIn = value * 60 * 60 * 1000;
+			expiresIn = expiresIn * 60 * 60 * 1000;
 		} else if (expirationString.includes('m')) {
-			expiresIn = value * 60 * 1000;
+			expiresIn = expiresIn * 60 * 1000;
 		} else if (expirationString.includes('s')) {
-			expiresIn = value * 1000;
+			expiresIn = expiresIn * 1000;
 		}
 
 		const expirationTime = issuedTime.getTime() + expiresIn;
